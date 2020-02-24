ea27ae43af0edd88bee7f21c700471efa43ee8ad
LillyWho <5427724+LillyWho@users.noreply.github.com>
2020-02-24 19:00:26 +0100

    Patch the PROBLEMATIC bit

--- ipc/shm.c
+++ ipc/shm.c
@@ -176,14 +176,15 @@ static void shm_rcu_free(struct rcu_head *head)
 	ipc_rcu_free(head);
 }
 
-static void shm_rcu_free(struct rcu_head *head)
+/*static void shm_rcu_free(struct rcu_head *head)
 {
 	struct ipc_rcu *p = container_of(head, struct ipc_rcu, rcu);
 	struct shmid_kernel *shp = ipc_rcu_to_struct(p);
 
 	security_shm_free(shp);
 	ipc_rcu_free(head);
-}
+}*/
+// I DID THE CHANGE HERE ^^^^^
 
 static inline void shm_rmid(struct ipc_namespace *ns, struct shmid_kernel *s)
 {
